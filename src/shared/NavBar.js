ee36fbce451eb603597e47af5a70374c9ddf48ee
Juan <juanjoseborrelli92@outlook.com>
2021-10-21 18:48:12 -0300

    Fix navbar

--- src/shared/NavBar.js
+++ src/shared/NavBar.js
@@ -1,8 +1,6 @@
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Dropdown from "react-bootstrap/Dropdown";
 import { InputSearch } from "./InputSearch";
 import { Rating } from "./Rating";
 
